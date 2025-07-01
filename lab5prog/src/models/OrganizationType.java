a489f2209c40e3843f0db63890a09635fd4e4609
Maks <nekrutenkomaksim59@gmail.com>
2025-07-01 13:38:46 +0300

    Add files via upload

---
+++ lab5prog/src/models/OrganizationType.java
@@ -0,0 +1,11 @@
+package models;
+
+
+// перечисление всех типов
+public enum OrganizationType {
+    COMMERCIAL,
+    GOVERNMENT,
+    TRUST,
+    PRIVATE_LIMITED_COMPANY,
+    OPEN_JOINT_STOCK_COMPANY;
+}
